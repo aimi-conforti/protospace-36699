1b26a0511b7b7d9d8b3cfee28f6b411744c5daaa
aimi-conforti <nozaru213@yahoo.co.jp>
2021-12-02 07:14:59 +0900

    ログイン実装　未完成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
@@ -10,4 +13,23 @@ class PrototypesController < ApplicationController
     Prototype.create(prototype_params)
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
